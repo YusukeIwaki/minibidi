c23bf7f094b646e43d1448667ebeffd99f47548a
YusukeIwaki <iwaki@i3-systems.com>
2024-09-30 08:09:15 +0900

    add feature: script.evaluate

--- lib/minibidi.rb
+++ lib/minibidi.rb
@@ -4,6 +4,7 @@ require 'minibidi/browser'
 require 'minibidi/browsing_context'
 require 'minibidi/browser_process'
 require 'minibidi/firefox_launcher'
+require 'minibidi/realm'
 
 module Minibidi
   class LaunchError < StandardError ; end
